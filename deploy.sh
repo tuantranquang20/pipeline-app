850f9b36d176545cb1c9481e83909ea6d5cf373c
tuantq-ttlab <tuantq@tokyotechlab.com>
2023-02-21 21:52:16 +0700

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cd $HOME/pnh-test
 git clone https://github.com/tuantranquang20/pnh-test.git .
 npm install
 npm run build
-rm -rf /var/www
-mkdir -p /var/www
-cp -R ./build /var/www
+rm -rf /var/www/html
+mkdir -p /var/www/html
+cp -R ./build /var/www/html
 echo "deployed"
