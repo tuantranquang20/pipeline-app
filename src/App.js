1cbeefff37f173fbbc7e9ab308d4449e30f4d223
tuantq-ttlab <tuantq@tokyotechlab.com>
2023-02-21 22:42:28 +0700

    u

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactzz
+          Learn Reactz
         </a>
       </header>
     </div>
