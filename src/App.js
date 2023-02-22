e42bd78595fed7becad665596ab1a594a78ee033
tuantq-ttlab <tuantq@tokyotechlab.com>
2023-02-22 09:54:39 +0700

    update pipeline

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Auto deloy
+          React js deploy
         </a>
       </header>
     </div>
